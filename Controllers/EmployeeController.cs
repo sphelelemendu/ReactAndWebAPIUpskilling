321ca7c52c6edc855f28eff9c4945ada6ed3214b
sphelelemendu <sphelelemendu@gmail.com>
2024-09-05 21:01:54 +0200

    Added API to get Employees from DB

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +13,19 @@ namespace WebAPI.Controllers
 {
     public class EmployeeController : ApiController
     {
+        public HttpResponseMessage Get()
+        {
+            DataTable employeeTable = new DataTable();
+            string query = @"SELECT EmployeeID, EmployeeName,MailID, CONVERT(varchar(10),DOJ,120) AS DOJ FROM dbo.Employees";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString) ) 
+            using ( var cmd = new SqlCommand(query, con) )
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(employeeTable);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, employeeTable);
+        }
     }
 }
