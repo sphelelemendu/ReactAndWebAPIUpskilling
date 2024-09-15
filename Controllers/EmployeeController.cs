aebbc9d582770d701404bb8109a71a1f0b045e77
sphelelemendu <sphelelemendu@gmail.com>
2024-09-15 08:21:07 +0200

    Added Post Method for Employee Controller but it is still not working fine

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
+
 
 namespace WebAPI.Controllers
 {
@@ -26,6 +28,34 @@ namespace WebAPI.Controllers
                 da.Fill(employeeTable);
             }
             return Request.CreateResponse(HttpStatusCode.OK, employeeTable);
+        }
+        [HttpPost]
+        public string Post(Employee emp)
+        {
+            try
+            {
+                DataTable employeeTable = new DataTable();
+                string query = @"INSERT INTO dbo.Employees(EmployeeName,Department,MailID,DOJ) VALUES(
+                '" +emp.EmployeeName + @"',
+                '" +emp.DepartmentName + @"',
+                '" +emp.MailID + @"',
+                '" +emp.DOJ +@"'";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(employeeTable);
+                }
+                return "Added Successfully";
+            }
+            
+            catch
+            {
+                return "Failed to add";
+            }
+
         }
     }
 }
