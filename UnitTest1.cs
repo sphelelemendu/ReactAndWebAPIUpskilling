4c60fe7aaa8d2249c99110400b2ab96f8b6ad4c8
sphelelemendu <sphelelemendu@gmail.com>
2025-02-02 21:46:09 +0200

    Initial set up of the test project

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,13 +1,25 @@
 namespace WebAPI.UnitTests
 {
-    public class UnitTest1
+    using Moq;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using Xunit;
+    using FluentAssertions;
+    using WebAPI.Models;
+    using WebAPI.Controllers;
+
+    public class EmployeeControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Get_ShouldReturnEmployeeList()
         {
-
-
 
         }
     }
+
 }
