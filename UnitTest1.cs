ae21b061113add194ec44404ccaa03d78c2235ed
sphelelemendu <sphelelemendu@gmail.com>
2025-02-04 21:01:41 +0200

    There is a fundamental issue with my xUnit testing project. My Testing project is running on .Net 8 while my app is .Net framework 4.7.2. This is causing compatibility issues and assembly references resolution issues. My decision is to completely upgrate my api into .Net 8

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,13 +12,43 @@ namespace WebAPI.UnitTests
     using FluentAssertions;
     using WebAPI.Models;
     using WebAPI.Controllers;
+    using WebAPI.Data_Layer_Service;
+
+
+
 
     public class EmployeeControllerTests
     {
         [Fact]
         public void Get_ShouldReturnEmployeeList()
         {
+            /*// Arrange
+            var mockService = new Mock<IEmployeeService>();
+            var employeeTable = new DataTable();
+            employeeTable.Columns.Add("EmployeeID", typeof(long));
+            employeeTable.Columns.Add("EmployeeName", typeof(string));
+            employeeTable.Columns.Add("Department", typeof(string));
+            employeeTable.Columns.Add("MailID", typeof(string));
+            employeeTable.Columns.Add("DOJ", typeof(string));
 
+            // Add a dummy row to the DataTable
+            employeeTable.Rows.Add(1, "Sphelele Mendu", "Digital Transformation", "sphelelem@dt.com", "2023-01-01");
+
+            mockService.Setup(service => service.GetEmployees()).Returns(employeeTable);
+
+            var controller = new EmployeesController(mockService.Object);
+
+            // Act
+            var result = controller.Get() as HttpResponseMessage;
+            var responseContent = result.Content.ReadAsAsync<DataTable>().Result;
+
+            // Assert
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            responseContent.Rows.Count.Should().Be(1);
+            responseContent.Rows[0]["EmployeeName"].Should().Be("Sphelele Mendu");
+            responseContent.Rows[0]["Department"].Should().Be("Digital Transformation");
+            responseContent.Rows[0]["MailID"].Should().Be("sphelelem@dt.com");*/
         }
     }
 
